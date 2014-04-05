e83cb1607799a760c7e38124fe37cbf801a0a2be
ArntIsaksen <arnt.isaksen@gmail.com>
2014-04-06 01:51:03 +0200

    printPaths() is now working.
    The printPaths() method prints the given folder, and the files included
    in that folder. Any folders or files with the ».»-prefix is excluded.

--- FileHandler.java
+++ FileHandler.java
@@ -1,3 +1,26 @@
-class FileHandler {
+import java.io.File;
+import java.io.IOException;
 
+class FileHandler {
+	String musicFolder;
+
+	boolean setMusicFolder(String name) {
+		musicFolder = name;
+		return true;
+	}
+
+	public static void printPaths(File folder){
+		String n = folder.getName();
+		if(!n.startsWith(".")) {
+			System.out.println(folder.getAbsoluteFile());
+			if(folder.isDirectory()) {
+				String[] subNote = folder.list();
+
+				for(String filename : subNote) {
+					printPaths(new File(folder, filename));
+				}
+			}
+		}
+
+	}
 }
--- QueueMyTune.java
+++ QueueMyTune.java
@@ -1,5 +1,8 @@
+import java.io.File;
+
 class QueueMyTune {
 	public static void main(String[] args) {
-
+		FileHandler fH = new FileHandler();
+		fH.printPaths(new File("/Users/arntisaksen/Documents/Under utvikling/QueueMyTune"));
 	}
 }
