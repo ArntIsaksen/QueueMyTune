f2181f1b022404b019190ba7d35a6a43d8c320f7
ArntIsaksen <arnt.isaksen@gmail.com>
2014-05-21 11:34:29 +0200

    Constructor and methods added to Artist and User.

--- Artist.java
+++ Artist.java
@@ -1,3 +1,17 @@
 class Artist {
 	String name;
+	ArrayList<Album> albums;
+
+	Artist(String name) {
+		this.name = name;
+		albums = new ArrayList<Album>();
+	}
+
+	void addAlbum(Album a) {
+		albums.add(a);
+	}
+
+	int nrOfAlbums() {
+		return albums.size();
+	}
 }
--- User.java
+++ User.java
@@ -1,8 +1,20 @@
 class User {
 	String id;
 	Track[] tracks;
+	int trackCount;
 
 	User() {
 		tracks = new Track[3];
+		trackCount = 0;
+	}
+
+	boolean addTrack(Track t) {
+		if (trackCount < tracks.length()) {
+			tracks[trackCount] = t;
+			trackCount++;
+			return true;
+		} else {
+			return false;
+		}
 	}
 }
